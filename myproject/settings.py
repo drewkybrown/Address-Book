42e55f557b9c88f434fab2b9728a8e27cb5291f3
Andrew Brown <andrew.brown.devs@gmail.com>
2024-02-07 16:22:13 -0500

    Congifure DATABASES to use dj-database

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,12 +80,14 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
